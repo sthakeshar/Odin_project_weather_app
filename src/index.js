384726b7ea29d3f7218266ac0bf6ada8fa71877e
sthakeshar <shresthakeshar291@gmail.com>
2026-02-21 20:20:38 +0900

    API fetched and successed

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
+/* eslint-disable no-unused-vars */
 import './style.css'
-alert('message from index.js')
+
+const apiKeyWeather = 'PU9U9K8Q54VQL5429LNJFVMUM';
+const apiKeyGif = 'hR1QMQqSHuBfxE0kNh92xoapGGrAnJbI&s';
+ const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/tokyo?key=${apiKeyWeather}`;
+
+async function getdata(){
+    try{
+        const respose=await fetch(url);
+        const data=await respose.json()
+        console.log(data)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+getdata();
